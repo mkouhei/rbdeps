680ab0b2995390a6f955e26f304b80d65fda050a
Kouhei Maeda <mkouhei@palmtb.net>
2015-12-04 06:14:37 +0900

    Applies simplecov.
    Signed-off-by: Kouhei Maeda <mkouhei@palmtb.net>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,24 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rbdeps'
+require 'simplecov'
+
+
+SimpleCov.start do
+  add_filter '/\.bundle/'
+  add_filter '/vendor/'
+end
+
+if ENV['CI']
+  require 'coveralls'
+  Coveralls.wear!
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+    Coveralls:SimpleCov::Formatter]
+  SimpleCov.start :test_frameworks
+end
+
+RSpec.configure do |config|
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
+end
